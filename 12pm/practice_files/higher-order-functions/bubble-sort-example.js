491828006a87b2f7b8707397219ba47283eb1215
WestMecRyan <ryan.morales@west-mec.org>
2024-09-23 12:55:53 -0700

    added log for sort

--- 12pm/practice_files/higher-order-functions/bubble-sort-example.js
+++ 12pm/practice_files/higher-order-functions/bubble-sort-example.js
@@ -1,5 +1,12 @@
 function test() {
     console.log('test');
 }
+const users = [{ id: 1, age: 25 }, { id: 2, age: 30 }, { id: 3, age: 18 }, { id: 4, age: 22 }, { id: 5, age: 19 }];
+function sortByAge(arr) {
+    let sortedArr = [...arr];
+    for (let i = 0; i < sortedArr.length; i++) {
+        console.log(sortedArr[i]);
+    }
+}
 
-module.exports = { test }
+module.exports = { test, users, sortByAge }
