1a72d2dad694d452bf5811196112d709bd0dc395
WestMecRyan <ryan.morales@west-mec.org>
2024-09-23 13:01:07 -0700

    finished sorted

--- 12pm/practice_files/higher-order-functions/bubble-sort-example.js
+++ 12pm/practice_files/higher-order-functions/bubble-sort-example.js
@@ -5,8 +5,16 @@ const users = [{ id: 1, age: 25 }, { id: 2, age: 30 }, { id: 3, age: 18 }, { id:
 function sortByAge(arr) {
     let sortedArr = [...arr];
     for (let i = 0; i < sortedArr.length; i++) {
-        console.log(sortedArr[i]);
+        // console.log(sortedArr[i]);
+        for (let j = 0; j < sortedArr.length - 1; j++) {
+            if (sortedArr[j].age > sortedArr[j + 1].age) {
+                let temp = sortedArr[j];
+                sortedArr[j] = sortedArr[j + 1];
+                sortedArr[j + 1] = temp;
+            }
+        }
     }
+    return sortedArr;
 }
 
 module.exports = { test, users, sortByAge }
