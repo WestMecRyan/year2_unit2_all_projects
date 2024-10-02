06a50a35612000acbd0d3ce2b8cecc005ba39133
WestMecRyan <ryan.morales@west-mec.org>
2024-10-02 12:39:01 -0700

    added css

--- 12pm/express-crud-user-form-app-9_23_24/server/index.js
+++ 12pm/express-crud-user-form-app-9_23_24/server/index.js
@@ -12,6 +12,7 @@ const clientPath = path.join(__dirname, '..', 'client/src');
 const dataPath = path.join(__dirname, 'data', 'users.json');
 const serverPublic = path.join(__dirname, 'public');
 // Middleware setup
+app.use(express.static(serverPublic));
 app.use(express.static(clientPath)); // Serve static files from client directory
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(express.json()); // Parse JSON bodies
